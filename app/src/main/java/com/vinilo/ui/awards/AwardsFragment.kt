f06729d958173ea3b3c8b87afe5dcea2fc56d4d6
Pablo Rivera <p.riverah@uniandes.edu.co>
2025-05-17 16:29:00 -0500

    Se añade EditText para añadir texto en el fragmento.

--- app/src/main/java/com/vinilo/ui/awards/AwardsFragment.kt
+++ app/src/main/java/com/vinilo/ui/awards/AwardsFragment.kt
@@ -1,6 +1,19 @@
 package com.vinilo.ui.awards
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.vinilo.view.R
+
 
 class AwardsFragment: Fragment()  {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_prize_create, container, false)
+    }
 }
