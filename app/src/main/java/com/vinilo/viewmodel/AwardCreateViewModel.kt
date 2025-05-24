702329ac2456bda7fe8cdd3731f7d9b83a52df4c
Pablo Rivera <p.riverah@uniandes.edu.co>
2025-05-24 16:59:56 -0500

    Features in create award (Reset the form and prompt a message when the awards was created.

--- app/src/main/java/com/vinilo/viewmodel/AwardCreateViewModel.kt
+++ app/src/main/java/com/vinilo/viewmodel/AwardCreateViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.viewModelScope
 import com.vinilo.data.repository.AwardRepository
 import com.vinilo.domain.model.PerformerPrizeRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,12 +15,15 @@ class AwardCreateViewModel @Inject constructor(
     private val repository: AwardRepository
 ) : ViewModel() {
 
+    private val _createAwardResponse = MutableStateFlow<String?>(null)
+    val createAwardResponse: StateFlow<String?> = _createAwardResponse
+
     fun createAward(nombre: String, desc: String, organizacion: String) {
         viewModelScope.launch {
             try {
                 val request = PerformerPrizeRequest(nombre, desc, organizacion)
                 val response = repository.createAward(request)
-                println(response)
+                _createAwardResponse.value = "Éxito: $response"
             } catch (e: Exception) {
                 println("Exception: ${e.message}")
             }
